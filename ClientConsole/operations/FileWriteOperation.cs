b25bf479f712f19c73de9fa174d2dcd5ab182c4f
manar.alkul <manar.alkul@connamix.com>
2025-03-31 09:54:21 +0300

    ...

--- ClientConsole/operations/FileWriteOperation.cs
+++ ClientConsole/operations/FileWriteOperation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClientConsole
+namespace ClientConsole.operations
 {
     public class FileWriteOperation
     {
@@ -20,42 +20,42 @@ namespace ClientConsole
         public FileWriteOperation(BenchmarkAppContext context)
         {
             this.context = context;
-            this.benchmarkClient = new BenchmarkClient(context);
+            benchmarkClient = new BenchmarkClient(context);
             csvFile = new($"{REQUEST_NAME}.csv");
             csvFile.resetFile();
         }
 
         public void run()
         {
-            foreach(var fileWriteConfig in context.config.fileWriteOperationConfigs)
+            foreach (var fileWriteConfig in context.config.fileWriteOperationConfigs)
             {
                 run(fileWriteConfig.fileSize, fileWriteConfig.iterations, fileWriteConfig.threads);
             }
         }
 
 
-        public void run(int fileSize,int iterations, int threads)
+        public void run(int fileSize, int iterations, int threads)
         {
             context.logger.Info($"Running Request '{REQUEST_NAME}' with file size: {fileSize}MB");
 
-            string relativeURL = "/api/Operations/fileWrite?fileSizeMB="+ fileSize;
+            string relativeURL = "/api/Operations/fileWrite?fileSizeMB=" + fileSize;
             List<BenchmarkRequestRecord> records = benchmarkClient.Run(REQUEST_NAME, HttpMethod.Get, relativeURL, "",
                 threads, iterations);
 
-/*            IEnumerable<ReportModel> avg = records
-                .Where(x=>x.StatusCode==200)
-                
-                .GroupBy(x => 
-                x.hostName,
-                x => x.responseBody,
-                (hostName, val) => new ReportModel(){ 
-                    hostName= hostName,
-                    avg=(val.Select(x=>Int32.Parse(x)).Average().ToInt()),
-                    fileSize= fileSize
-                    }
-                );*/
+            /*            IEnumerable<ReportModel> avg = records
+                            .Where(x=>x.StatusCode==200)
 
-            IEnumerable<FileWriteOperationModel>  operationRecords = records.Select(x => new FileWriteOperationModel()
+                            .GroupBy(x => 
+                            x.hostName,
+                            x => x.responseBody,
+                            (hostName, val) => new ReportModel(){ 
+                                hostName= hostName,
+                                avg=(val.Select(x=>Int32.Parse(x)).Average().ToInt()),
+                                fileSize= fileSize
+                                }
+                            );*/
+
+            IEnumerable<FileWriteOperationModel> operationRecords = records.Select(x => new FileWriteOperationModel()
             {
                 period = x.period,
                 responseBody = x.responseBody,
@@ -76,12 +76,12 @@ namespace ClientConsole
     {
         public required int fileSize { get; set; }
     }
-/*
-    internal class ReportModel
-    {
-        public required string hostName { get; set;}
-        public required int avg { get; set;}
-        public required int fileSize { get; set;}
-    }
-*/
+    /*
+        internal class ReportModel
+        {
+            public required string hostName { get; set;}
+            public required int avg { get; set;}
+            public required int fileSize { get; set;}
+        }
+    */
 }
